38ec7711a819a33ab7fa8e9fcf008fa56b322004
mmiskatul <masabimiskat@gmail.com>
2026-07-16 08:57:39 +0600

    fix: Remove TechStackGrid from homepage - only show on /technology page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import WhatWeDo from '../components/WhatWeDo';
 import WhoWeAre from '../components/WhoWeAre';
 import WhyUs from '../components/WhyUs';
 import Capabilities from '../components/Capabilities';
-import TechStackGrid from '../components/TechStackGrid';
 import Stats from '../components/Stats';
 import CTA from '../components/CTA';
 import Footer from '../components/Footer';
@@ -31,10 +30,6 @@ export default function Home() {
         
         <ScrollReveal animationClass="reveal-scale" threshold={0.05}>
           <WhyUs />
-        </ScrollReveal>
-        
-        <ScrollReveal animationClass="reveal-slide-up" threshold={0.05}>
-          <TechStackGrid />
         </ScrollReveal>
         
         <ScrollReveal animationClass="reveal-slide-up" threshold={0.05}>
