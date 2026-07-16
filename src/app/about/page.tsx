25465d7cb7779e8371df2dce64c2869944846bf9
mmiskatul <masabimiskat@gmail.com>
2026-07-16 09:23:14 +0600

    remove: Remove Careers job listings section from about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,6 @@ import Navbar from '../../components/Navbar';
 import AboutHero from '../../components/AboutHero';
 import MissionVision from '../../components/MissionVision';
 import CoreValues from '../../components/CoreValues';
-import Careers from '../../components/Careers';
 import Footer from '../../components/Footer';
 
 export default function AboutPage() {
@@ -14,7 +13,6 @@ export default function AboutPage() {
         <AboutHero />
         <MissionVision />
         <CoreValues />
-        <Careers />
       </main>
       <Footer />
     </>
